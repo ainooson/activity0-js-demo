ca751af6e7c5c8f89d72b4d10cb7a3266f1f2639
ainooson <ojidaddy@gmail.com>
2024-01-23 12:34:30 -1200

    This is activity 0 JS Demo

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  alert('Hello, World!');
+  console.log('Hello, World!');
+  document.querySelector('h1').textContent = 'Goodbye, World!';
+});
